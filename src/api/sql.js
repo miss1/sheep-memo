e763b2e630fafbbf2d05b15bc66619e9d3b6995e
liling yang <yangll2525@163.com>
2022-08-25 09:24:37 +0800

    Add Special day timeline

--- src/api/sql.js
+++ src/api/sql.js
@@ -1,3 +1,4 @@
 export const QUERY_USERS = 'select * from login'
 export const QUERY_SPECIAL_DAYS = 'select * from special order by time desc'
+export const QUERY_SPECIAL_DAYS_Timeline = 'select * from special where type != 1 order by time asc'
 export const QUERY_LETTERS = 'select * from letter order by time desc'
--- src/views/special.js
+++ src/views/special.js
@@ -3,12 +3,23 @@ import '../assets/special.css'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { queryData } from '../api/connectSqlite'
-import { QUERY_SPECIAL_DAYS } from '../api/sql'
+import { QUERY_SPECIAL_DAYS, QUERY_SPECIAL_DAYS_Timeline } from '../api/sql'
 import React, { useEffect, useState } from "react";
+import Fab from '@mui/material/Fab';
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
+import Typography from '@mui/material/Typography';
 import moment from "moment";
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 
 function Special() {
   const [list, setList] = useState([]);
+  const [timeline, setTimeline] = useState([]);
+  const [showTimeLine, setShowTimeLine] = useState(false);
   useEffect( () => {
     let data = queryData(QUERY_SPECIAL_DAYS);
     data = data.map(val => {
@@ -23,27 +34,53 @@ function Special() {
       return val;
     });
     setList(data);
+    setTimeline(queryData(QUERY_SPECIAL_DAYS_Timeline));
   },[setList]);
+
+  const doAction = () => {
+    setShowTimeLine(!showTimeLine);
+  }
+
   return (
     <div className="page">
       <Header/>
       <div className="content-page">
-        <div className="page-scroll special-page">
-          {list.map((item) => (
-            <Card key={item.title} sx={{ width: { xs: '100%', md: '49%' }, margin: '5px 0' }}>
-              <CardContent sx={{ m: 0, p: 0 }}>
-                <div className={item.type === 1 ? 'special-head-down' : 'special-head-up'}>
-                  <p>{item.title}</p>
-                  <p>{item.type === 1 ? 'Arrives in' : 'Already'}</p>
-                </div>
-                <p className="special-content">{item.days}</p>
-                <p className="special-sub-content">{item.duration}</p>
-                <p className="special-foot">{item.time}</p>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+        {showTimeLine
+          ? <div className="page-scroll special-timeline">
+            <Timeline position="alternate">
+              {timeline.map((item) => (
+                <TimelineItem key={item.title}>
+                  <TimelineSeparator>
+                    <TimelineDot color='success' />
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>{item.title} ({item.time})</TimelineContent>
+                </TimelineItem>
+              ))}
+            </Timeline>
+            <Typography variant="string" sx={{textAlign: 'center'}}>to be continued</Typography>
+          </div>
+          : <div className="page-scroll special-page">
+            {list.map((item) => (
+              <Card key={item.title} sx={{width: {xs: '100%', md: '49%'}, margin: '5px 0'}}>
+                <CardContent sx={{m: 0, p: 0}}>
+                  <div className={item.type === 1 ? 'special-head-down' : 'special-head-up'}>
+                    <p>{item.title}</p>
+                    <p>{item.type === 1 ? 'Arrives in' : 'Already'}</p>
+                  </div>
+                  <p className="special-content">{item.days}</p>
+                  <p className="special-sub-content">{item.duration}</p>
+                  <p className="special-foot">{item.time}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        }
       </div>
+      <Fab onClick={doAction} color={showTimeLine ? 'warning' : 'primary'}
+           sx={{position: 'absolute', bottom: '5%', right: '10%'}}>
+        <SwapHorizIcon/>
+      </Fab>
     </div>
   );
 }
